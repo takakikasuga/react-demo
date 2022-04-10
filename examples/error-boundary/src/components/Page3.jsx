84358ac17ba6e4d9c48b69e14da34353b6d20500
takakikasuga <takaki.soccer1@gmail.com>
2022-04-10 15:33:02 +0900

    :sparkles: エラーリカバリー（状態を回復する場合）

--- examples/error-boundary/src/components/Page3.jsx
+++ examples/error-boundary/src/components/Page3.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import ErrorFallback from './ErrorFallback';
 
@@ -7,18 +8,32 @@ function onError(error, info) {
 }
 
 function ThrowError() {
-  if (new Date().getMilliseconds() % 2 === 0) {
-    throw new Error('Page3Child Throw Error');
-  }
-  return <p>not throw error</p>;
+  throw new Error('Page3Child Throw Error');
 }
 
 function Page3Child() {
+  const [throwError, setThrowError] = React.useState(false);
+
+  const onClick = () => {
+    setThrowError((preThrowError) => !preThrowError);
+  };
+
+  const onReset = () => {
+    setThrowError((pre) => !pre);
+  };
+
   return (
-    <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError}>
+    <ErrorBoundary
+      FallbackComponent={ErrorFallback}
+      onError={onError}
+      onReset={onReset}
+      resetKeys={[throwError]}>
       <div style={{ backgroundColor: '#DEB331' }}>
-        <h5>Page3Child</h5>
-        <ThrowError />
+        <h5>Page4</h5>
+        <button type='button' onClick={onClick}>
+          Throw Error
+        </button>
+        {throwError && <ThrowError />}
       </div>
     </ErrorBoundary>
   );
