899053d1846e75a67de678ad73e4cd776e09fe0c
takakikasuga <takaki.soccer1@gmail.com>
2022-04-10 15:23:59 +0900

    :sparkles:  エラー時にログを吐くように実装

--- examples/error-boundary/src/App.js
+++ examples/error-boundary/src/App.js
@@ -1,13 +1,23 @@
 import Page1 from './components/Page1';
 import Page2 from './components/Page2';
 import Page3 from './components/Page3';
+import { ErrorBoundary } from 'react-error-boundary';
+import ErrorFallback from './components/ErrorFallback';
+
+const onError = (error, info) => {
+  // ここでログ出力などを行う
+  console.log('error.message', error.message);
+  console.log('info.componentStack:', info.componentStack);
+};
 
 function App() {
   return (
     <>
       <Page1 />
       <Page2 />
-      <Page3 />
+      <ErrorBoundary FallbackComponent={ErrorFallback} onError={onError}>
+        <Page3 />
+      </ErrorBoundary>
     </>
   );
 }
--- examples/error-boundary/src/components/Page3.jsx
+++ examples/error-boundary/src/components/Page3.jsx
@@ -1,14 +1,10 @@
-import { ErrorBoundary } from 'react-error-boundary';
-import ErrorFallback from './ErrorFallback';
 import Page3Child from './Page3Child';
 
 function Page3() {
   return (
     <div style={{ backgroundColor: '#DFD35F' }}>
       <h3>Page3</h3>
-      <ErrorBoundary FallbackComponent={ErrorFallback}>
-        <Page3Child />
-      </ErrorBoundary>
+      <Page3Child />
     </div>
   );
 }
